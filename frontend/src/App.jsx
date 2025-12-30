f04cffaaaa51e43167c2e1af61742ecf82a69588
kavinkumar1603 <kavin88701@gmail.com>
2025-12-30 22:41:34 +0530

    add RoleSelection component route to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './components/LandingPage.jsx';
+import RoleSelection from './components/RoleSelection.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
-        
+        <Route path="/home" element={<RoleSelection />} />
       </Routes>
     </>
   )
